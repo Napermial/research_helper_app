94da7f693c646c25f44849d254a3037ca1fc64ea
Napermial <h>
2021-04-04 17:35:30 +0200

    login form added

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-from .models import Item, Research
-
-admin.site.register(Item)
-admin.site.register(Research)
--- users/views.py
+++ users/views.py
@@ -3,30 +3,37 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 
 def index(request):
-    return render(request, "home.html")
+    return render(request, "users/home.html")
 
 
 @login_required
 def user(request, user_id):
-    response = "You're looking at the profile of user %s."
-    return render(request, "user.html")
+    return render(request, "users/user.html")
 
 
 @login_required
 def researches(request):
-    return render(request, "user.html", {'n' : range(20) })
+    return render(request, "users/user.html", {'n': range(20)})
+
+
+def login_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user_auth = authenticate(request, username=username, password=password)
+    if user_auth is not None:
+        login(request, user)
+    else:
+        return HttpResponse('no')
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponse('logged out')
 
 
 def register(request):
-    return HttpResponse('start')
-
-
-def login(request):
-    return HttpResponse('start')
-
-
-def logout(request):
     return HttpResponse('start')
