c6e73efcbd6927708187c35e13ba70f9c7293dd0
Napermial <hatvani9823@gmail.com>
2021-07-11 19:35:46 +0200

    test structure example added

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class FileHandling(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_valid_format_is_handled(self) -> bool:
+        """files that follow the file structure doesn't throw an exception"""
+        return True
