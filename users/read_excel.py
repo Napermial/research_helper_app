1573721c11d47cd5022a851ac6172e0bfed572a1
Napermial <hatvani9823@gmail.com>
2021-07-11 17:32:38 +0200

    docstrings added to read excel with type hinting

--- users/read_excel.py
+++ users/read_excel.py
@@ -1,10 +1,11 @@
-from pandas import read_excel
 import openpyxl
 from io import BytesIO
-from .models import Factor, Level, Item, ItemLevel
+from .models import Factor, Level, Item, ItemLevel, Experiment
 
 
 class Line:
+    """class to keep track of the lines of the read excel document"""
+
     def __init__(self, stimulus, pre_context, post_context, lexicalization, levels):
         self.stimulus = stimulus
         self.pre_context = pre_context
@@ -19,7 +20,12 @@ class Line:
         return self.stimulus
 
 
-def read_file(input_file):
+def read_file(input_file: object) -> (Line, list):
+    """
+    reads the file and composes the structure of the items
+    :param input_file: the uploaded file
+    :returns the lines as Line(s), the structure of the factors in the file
+    """
     factors_in_workbook = {}
     workbook = openpyxl.load_workbook(filename=BytesIO(input_file.read()), data_only=True).active
     position = 4
@@ -33,7 +39,12 @@ def read_file(input_file):
     return lines, factors_in_workbook
 
 
-def create_item(line):
+def create_item(line: Line) -> Item:
+    """
+    creates an Item and saves it to the database
+    :param line :  a Line object containing the data of the line
+    :returns the Line to add the connected Level
+    """
     item = Item()
     item.item_text = line.stimulus
     item.pre_item_context = line.pre_context
@@ -43,7 +54,13 @@ def create_item(line):
     return item
 
 
-def put_into_db(file, levels, experiment):
+def put_into_db(file: dict, levels: list, experiment: Experiment):
+    """
+    inserts the lines to the database
+    :param file: the read file
+    :param levels: list of the created levels with metadata
+    :param experiment: the Experiment object to connect to
+    """
     for line in file:
         item = create_item(line)
         item.experiment_id = experiment
@@ -58,7 +75,13 @@ def put_into_db(file, levels, experiment):
                     break
 
 
-def insert_factors(schema, experiment_id):
+def insert_factors(schema: dict, experiment_id: Experiment) -> [(Level, str, str)]:
+    """
+    inserts the factors and levels to the database
+    :param schema: the input structure of levels and factors from the web
+    :param experiment_id: the created Experiment
+    :return: list with level reference, name and factor name
+    """
     levels = []
     for factor in schema:
         db_factor = Factor(name=factor["name"], experiment_id=experiment_id)
--- users/utils.py
+++ users/utils.py
@@ -2,6 +2,7 @@ from itertools import tee
 
 
 def pairwise(seq):
+    """:returns the next iterator of the sequence"""
     a, b = tee(seq)
     next(b)
     return zip(a, b)
